da067aa383666aedefedb6529d7f7e731c7ff572
lolrem <qpple6@slogup.com>
2024-08-01 12:38:35 +0900

    build: init eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        pathGroups: [
+          { pattern: 'react*', group: 'builtin', position: 'before' },
+          { pattern: '@src/**', group: 'external', position: 'after' },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: { order: 'asc' },
+      },
+    ],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: true, classes: true, variables: false },
+    ],
+    'react/require-default-props': 'off',
+    'react/jsx-curly-brace-presence': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
