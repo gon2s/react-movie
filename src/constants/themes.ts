20da8a0311681b0bfd3db8146c9a6a8b970cc478
gon2s <ghidhi81@gmail.com>
2024-08-22 12:14:43 +0900

    ui: update themes color

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -1,8 +1,18 @@
 import { DefaultTheme } from 'styled-components';
 
 export const colors = {
-  WHITE: '#FFFFFF',
-  BLACK: '#000000',
+  red: '#E51013',
+  black: {
+    primary: '#000000',
+    veryDark: '#141414',
+    darker: '#181818',
+    lighter: '#2F2F2F',
+  },
+  white: {
+    primary: '#FFFFFF',
+    lighter: '#fff',
+    darker: '#e5e5e5',
+  },
 };
 
 export const themes: DefaultTheme = {
