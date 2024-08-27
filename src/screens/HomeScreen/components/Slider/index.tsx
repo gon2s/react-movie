b8744b576eb16f1edf28c178407ec39620b5296e
gon2s <ghidhi81@gmail.com>
2024-08-27 18:52:17 +0900

    feat: add HomeScreen slider scale animation

--- src/screens/HomeScreen/components/Slider/index.tsx
+++ src/screens/HomeScreen/components/Slider/index.tsx
@@ -12,11 +12,21 @@ function Slider({ data, style }: SliderProps) {
   return (
     <S.SliderItem
       style={style}
+      variants={S.sliderVariant}
+      whileHover={'hover'}
+      initial={'initial'}
+      transition={{
+        type: 'tween',
+      }}
       bgImg={getImgUrl({
         id: data?.backdrop_path || '',
         format: 'w500',
       })}
-    />
+    >
+      <S.InfoWrapper variants={S.infoVariant}>
+        <h4>{data.title}</h4>
+      </S.InfoWrapper>
+    </S.SliderItem>
   );
 }
 
--- src/screens/HomeScreen/components/Slider/styles.ts
+++ src/screens/HomeScreen/components/Slider/styles.ts
@@ -1,5 +1,20 @@
-import { motion } from 'framer-motion';
+import { motion, Variants } from 'framer-motion';
 import { styled } from 'styled-components';
+
+export const sliderVariant: Variants = {
+  initial: {
+    scale: 1,
+  },
+  hover: {
+    cursor: 'pointer',
+    scale: 1.2,
+    transition: {
+      type: 'tween',
+      delay: 0.6,
+      duration: 0.1,
+    },
+  },
+};
 
 export const SliderItem = styled(motion.div)<{ bgImg?: string }>`
   cursor: pointer;
@@ -9,4 +24,41 @@ export const SliderItem = styled(motion.div)<{ bgImg?: string }>`
   background-image: url(${({ bgImg }) => bgImg});
   background-size: cover;
   background-position: center center;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
+  transform-origin: center 100%;
+`;
+
+export const infoVariant: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  hover: {
+    type: 'tween',
+    opacity: 1,
+    transition: {
+      delay: 0.6,
+      duration: 0.2,
+    },
+  },
+};
+
+export const InfoWrapper = styled(motion.div)`
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  opacity: 0;
+  box-sizing: border-box;
+  /* border: 1px solid red; */
+  padding: 20px;
+  background-color: ${({ theme }) => theme.colors.black.lighter};
+  h4 {
+    text-align: center;
+    font-size: 18px;
+    color: ${({ theme }) => theme.colors.white.darker};
+  }
 `;
