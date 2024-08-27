1aac64630c72c10b4b6c7e4972f6e4b10cc68138
gon2s <ghidhi81@gmail.com>
2024-08-27 12:16:43 +0900

    setting axios type

--- src/networks/types.ts
+++ src/networks/types.ts
@@ -4,6 +4,18 @@ export interface RequestParams {
   method: HttpMethod;
   url: string;
   queryParams?: Record<string, unknown>;
+  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
   requestBody?: FormData | unknown | string;
   isMultipart?: boolean;
+}
+
+export interface Response<T> {
+  dates: {
+    maximum: number;
+    minimum: number;
+  };
+  page: number;
+  results: T[];
+  total_pages: number;
+  total_results: number;
 }
